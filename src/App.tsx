caa1e089919b4956777ce226532bf44d97453b18
kevin083177 <kevin083177@gmail.com>
2024-11-14 17:15:58 +0800

    　

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
 import { Routes, Route, Link } from "react-router-dom"; // +
 import bg from '/Rabbit_face.jpg'
+import './App.css';
+
 // +
 function App() {
   return (
+      <Routes>  
+        <Route path="/test-react-pages" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+  );
+}
+
+// + Home() & About()
+function Home() {
+  return (
     <>
-      <section>
+      <main>
         <h2>Welcome to the homepage!</h2>
         <p>You can do this, I believe in you.</p>
         <img src={bg}></img>
-      </section>
+      </main>
       <nav>
         <Link to="/about">About</Link>
       </nav>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
     </>
   );
 }
 
-
 function About() {
   return (
     <>
-      <section>
+      <main>
         <h2>Who are we?</h2>
         <p>
           That feels like an existential question, don't you
           think?
         </p>
-      </section>
+      </main>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/test-react-pages">Home</Link>
       </nav>
     </>
   );
