b28adef7766e16634519c71fb817c096fe9dfbbd
kevin083177 <kevin083177@gmail.com>
2024-11-14 15:44:24 +0800

    456

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Link } from "react-router-dom"; // +
+import bg from '/Rabbit_face.jpg'
 // +
 function App() {
   return (
     <>
-      <main>
+      <section>
         <h2>Welcome to the homepage!</h2>
         <p>You can do this, I believe in you.</p>
-      </main>
+        <img src={bg}></img>
+      </section>
       <nav>
         <Link to="/about">About</Link>
       </nav>
@@ -17,32 +19,17 @@ function App() {
   );
 }
 
-// + Home() & About()
-// @ts-ignore
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
 
 function About() {
   return (
     <>
-      <main>
+      <section>
         <h2>Who are we?</h2>
         <p>
           That feels like an existential question, don't you
           think?
         </p>
-      </main>
+      </section>
       <nav>
         <Link to="/">Home</Link>
       </nav>
